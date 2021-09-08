fc0df09e843a0143d1b9e2e235270458872488c0
Lyrai <x355x02c@live.com>
2021-09-08 20:44:38 +0300

    added registration and getting user by id

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,26 @@ mod user;
 
 use chat::Chat;
 use mongodb::{
+    bson,
     bson::doc,
     options::{ClientOptions, FindOptions},
     Client,
+    Collection
 };
 use rocket::{futures::TryStreamExt, http::Status, response::status, tokio, Build, Rocket, State};
 use std::sync::Arc;
 use user::{User, Users};
-use rocket::response::content::Json;
+use serde_json as json;
+use serde::Serialize;
+use rocket::serde::Serializer;
+use std::borrow::Borrow;
+use std::error::Error;
+
+macro_rules! log_id {
+    ($db: ident) => {
+        $db.database("users").collection::<User>("log_id")
+    };
+}
 
 #[macro_use]
 extern crate rocket;
@@ -19,43 +31,82 @@ extern crate rocket;
 async fn launch() -> Rocket<Build> {
     start_chat();
     let mut db_client_options =
-        ClientOptions::parse("mongodb+srv://admin:BsJf03UnsmgQnXeW@cluster0.us09s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority").await.unwrap();
+        ClientOptions::parse("mongodb+srv://server:jRk3JcqhsOXsT4RC@cluster0.us09s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+            .await
+            .unwrap();
 
     db_client_options.app_name = Some("Chat".to_string());
     let db_client = Client::with_options(db_client_options).unwrap();
+    let users = get_users_internal(&db_client).await.unwrap();
 
     rocket::build()
-        .manage(Users::new())
+        .manage(users)
         .manage(db_client)
         .mount("/", routes![register, get_users])
 }
 
 #[post("/register")]
-fn register(info: User, users: &State<Client>) -> status::Custom<&'static str> {
-    //if let Err(e) = users.add(info) {
-    //    eprintln!("User {} already exists", e);
-    //    return status::Custom(Status::BadRequest, "User with given login is already registered");
-    //}
-    //
-    //println!("Registered {}", info.login);
-    status::Custom(Status::Accepted, "")
+async fn register(info: User, db_client: &State<Client>, users: &State<Users>) -> status::Custom<String> {
+    let db = log_id!(db_client);
+    let found = db.find_one(doc! {"login": info.login.clone()}, None).await;
+
+    if let Ok(Some(_)) = found {
+        return status::Custom(Status::Conflict, format!("User with login {} already exists", info.login));
+    } else if let Err(e) = found {
+        eprintln!("Registration error: {:?}", e);
+        return status::Custom(Status::BadGateway, "Error occurred during registration".to_owned());
+    }
+
+    let id = db.count_documents(None, None).await.unwrap();
+
+    let new = User {
+        login: info.login,
+        id: id as i32
+    };
+
+    let insert_result = db.insert_one(new.clone(), None).await;
+
+    if let Err(e) = insert_result {
+        eprintln!("Error adding db: {:?}", e);
+        return status::Custom(Status::BadGateway, "Error occurred while adding to db".to_owned())
+    }
+
+    users.add(new);
+    status::Custom(Status::Accepted, "Success".to_owned())
 }
 
 #[get("/get_users")]
 async fn get_users(db_client: &State<Client>) -> String {
-    let db = db_client.database("users").collection::<User>("log_id");
-    let mut users = db.find(doc! {}, FindOptions::default()).await.unwrap();
+    let result = get_users_internal(db_client).await;
+    if let Err(e) = result {
+        eprintln!("Error retrieving users: {:?}", e);
+        return "Internal server error".to_owned();
+    }
+
+    json::to_string(&result.unwrap()).unwrap()
+}
+
+async fn get_users_internal(db_client: &Client) -> Result<Users, Box<dyn Error>> {
+    let db = log_id!(db_client);
+
+    let mut users = db.find(None, None).await?;
     let result = Users::new();
-    while let Some(user) = users.try_next().await.unwrap() {
+    while let Some(user) = users.try_next().await? {
         result.add(user);
     }
 
-    serde_json::to_string(&result).unwrap()
+    Ok(result)
+}
+
+#[get("/get/<id>")]
+fn get_user_by_id(id: i32, users: &State<Users>) -> Option<User> {
+    users.get(id)
 }
 
 fn start_chat() {
     tokio::spawn(async {
-        let listener = tokio::net::TcpListener::bind("127.0.0.1:1488")
+        //let port = std::env::var("PORT").unwrap();
+        let listener = tokio::net::TcpListener::bind("0.0.0.0:1488")
             .await
             .unwrap();
         let chat = Arc::new(Chat::new());
--- src/user.rs
+++ src/user.rs
@@ -1,9 +1,11 @@
-use rocket::http::Status;
+use rocket::http::{Status, Header};
 use rocket::request::{FromRequest, Outcome};
-use rocket::Request;
+use rocket::{Request, Response};
 use serde::{Deserialize, Serialize};
 use std::cell::RefCell;
 use std::sync::Mutex;
+use rocket::response::{Responder, Result};
+use std::io::Cursor;
 
 macro_rules! unwrap_mutex {
     ($id: expr, $m: ident) => {
@@ -14,7 +16,7 @@ macro_rules! unwrap_mutex {
 #[derive(Clone, Serialize, Deserialize)]
 pub struct User {
     pub login: String,
-    pub id: u8,
+    pub id: i32,
 }
 
 #[async_trait]
@@ -34,7 +36,21 @@ impl<'r> FromRequest<'r> for User {
     }
 }
 
-#[derive(Serialize)]
+impl<'r, 'o: 'r> Responder<'r, 'o> for User {
+    fn respond_to(self, request: &'r Request<'_>) -> Result<'o> {
+        let json = serde_json::to_string(&self).unwrap();
+
+        let response = Response::build()
+            .status(Status::Found)
+            .sized_body(json.len(), Cursor::new(json))
+            .raw_header("Content-Type", "application/json")
+            .finalize();
+
+        Ok(response)
+    }
+}
+
+#[derive(Serialize, Deserialize)]
 pub struct Users {
     pub users: Mutex<RefCell<Vec<User>>>,
 }
@@ -46,10 +62,10 @@ impl Users {
         }
     }
 
-    pub fn get(&self, login: String) -> Option<User> {
+    pub fn get(&self, id: i32) -> Option<User> {
         unwrap_mutex!(self.users, borrow)
             .iter()
-            .find(|x| x.login == login)
+            .find(|x| x.id == id)
             .cloned()
     }
 
